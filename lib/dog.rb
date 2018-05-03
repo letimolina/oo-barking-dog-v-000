bee1e27579727cd030b9c2aee266016e2e15e0ec
Leticia Molina <letimolina@gmail.com>
2018-05-03 00:48:32 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,16 @@
 class Dog
-
+def initialize(name)
+    @name = name
+  end
+ 
+  def name=(name)
+    @name = name
+  end
+ 
+  def name
+    @name
+  end
+end
  fido = Dog.new
 fido.name = "Fido"
 
