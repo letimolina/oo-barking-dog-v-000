10a39e3aeeb15b1738d845a957fdc85b81e4eb25
Leticia Molina <letimolina@gmail.com>
2018-05-03 00:45:51 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Dog
+  def initialize(breed)
+    @breed = breed
+  end
+ 
+  def breed=(breed)
+    @breed = breed
+  end
+ 
+  def breed
+    @breed
+  end
+end# Your code goes here!
